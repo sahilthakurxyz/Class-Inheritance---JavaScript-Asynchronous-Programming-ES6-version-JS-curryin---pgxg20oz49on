4eac60a9481998daf3f928f4b044a51d89bd9c6d
Sahil <sahilthakurxyz@gmail.com>
2023-01-08 03:44:11 +0530

    updated

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,10 @@
 // declare class
-
+class Shape {
+  constructor() {
+    this.color = "green";
+  }
+  drawShape() {}
+  calculateArea() {}
+}
+module.exports = Shape;
 // export class using module.exports
